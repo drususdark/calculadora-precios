b67e52d09155572b5253834ca3c5bc586aa2d42f
drususdark <63212131+drususdark@users.noreply.github.com>
2025-06-07 22:49:24 -0400

    add speed insights para react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Calculator } from './components/Calculator'
 import { Footer } from './components/Footer'
 import './App.css'
 import { Analytics } from '@vercel/analytics/react' // <-- Agregado
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
